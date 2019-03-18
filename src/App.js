10e8f42858d35764650aa07ce258107058618080
l00pen <lovisa.skeppe@gmail.com>
2019-03-18 12:15:39 +0100

    Add contact

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const email = 'lovisa@skeppeconsulting.com';
     return (
       <div className={'App'}>
         <Background />
         <div className={'App__border'}>
           <h1 className={'App__header'}>SKEPPE CONSULTING</h1>
+          <p>
+            {`contact: ${email}`}
+          </p>
         </div>
       </div>
     );
