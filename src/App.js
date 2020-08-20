b0df956dfcf37244cf0872bbda267200e8cea5f7
l00pen <lovisa.skeppe@gmail.com>
2020-08-20 11:33:26 +0200

    Add themes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled, { ThemeProvider } from 'styled-components'
 import Background from './Background';
 import Projects from './Projects';
 
+import { lightTheme, darkTheme } from "./Components/Themes"
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import {
@@ -30,14 +31,14 @@ const AppContent = styled.main`
 
 const Divider = styled.hr`
   width: 50%;
-  background-color: rgba(245,245,245,0.7);
-  color: rgba(245,245,245,0.7);
+  background-color: ${({ theme }) => theme.divider};
+  color: ${({ theme }) => theme.divider};
   border: 0;
   height: 1px;
 `;
 
 const ContentSection = styled.section`
-  background-color: rgba(245,245,245,0.7);
+  background-color: ${({ theme }) => theme.background};
   flex: 1 0 auto;
   padding: 0.5em;
   min-height: 20em;
@@ -46,6 +47,7 @@ const ContentSection = styled.section`
   align-items: center;
   justify-content: center;
   box-sizing: border-box;
+  transition: all 0.50s linear;
 `;
 
 const ContentSectionContent = styled.div`
@@ -70,7 +72,7 @@ const ContactCards = styled.a`
   }
 
   @media only screen and (min-width: 768px) {
-
+    min-width: 33%;
   }
 `
 
@@ -99,50 +101,69 @@ const Contact = styled.div`
 `;
 
 
-class App extends Component {
-  render() {
-    const email = 'lovisa.skeppe@gmail.com';
-    const github = 'https://github.com/l00pen';
-    const profileGoogleDocs = 'https://docs.google.com/document/d/e/2PACX-1vRNRfupR3nSkqAGKG9A2FIqJNZ-OoiRYxEe25NnBhUEfODNbG4dOBJs-XlFGPyuErHST0DkopUos1SK/pub';
+const App = () => {
+  const email = 'lovisa.skeppe@gmail.com';
+  const github = 'https://github.com/l00pen';
+  const profileGoogleDocs = 'https://docs.google.com/document/d/e/2PACX-1vRNRfupR3nSkqAGKG9A2FIqJNZ-OoiRYxEe25NnBhUEfODNbG4dOBJs-XlFGPyuErHST0DkopUos1SK/pub';
 
-    return (
-      <ThemeProvider theme={{ mode: 'light', layout: 'cozy' }}>
-        <AppRoot>
-          <Background />
-          <Header />
-          <AppContent>
-            <ContentSection>
-              <AppTitle>SKEPPE CONSULTING</AppTitle>
-              <ContentSectionContent>
-                <Text>
-                  Experience in building and maintaining large web applications including CI/CD pipelines, hosting and integrating with internal and external API's. Optimizing for speed, scale and code readability.
-                </Text>
-                <Text>
-                  Worked with JavaScript and in the React ecosystem since 2014 and have a passion for modularization and readable code.
-                </Text>
-              </ContentSectionContent>
-            </ContentSection>
-            <Divider />
-            <ContentSection>
-              <Title>Contact</Title>
-              <ContentSectionContent>
-                <Contact>
-                  <ContactCards>{email}</ContactCards>
-                  <ContactCards href={github}>github profile</ContactCards>
-                  <ContactCards href={profileGoogleDocs}>profile</ContactCards>
-                </Contact>
-              </ContentSectionContent>  
-            </ContentSection>
-            <Divider />
-            <ContentSection>
-              <Projects />
-            </ContentSection>
-          </AppContent>
-          <Footer />
-        </AppRoot>
-      </ThemeProvider>
-    );
+  const [theme, setTheme] = useState('light');
+  
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light')
   }
+
+
+  useEffect(() => {
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+    const themeMatcher = () => {
+      setTheme(prefersDarkScheme.matches ? 'dark' : 'light')
+    }
+
+    themeMatcher();
+    prefersDarkScheme.addListener(themeMatcher);
+
+    return () => {
+      prefersDarkScheme.removeListener(themeMatcher)
+    }
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <AppRoot>
+        <Background />
+        <Header />
+        <AppContent>
+          <ContentSection>
+            <AppTitle>SKEPPE CONSULTING</AppTitle>
+            <ContentSectionContent>
+              <Text>
+                Experience in building and maintaining large web applications including CI/CD pipelines, hosting and integrating with internal and external API's. Optimizing for speed, scale and code readability.
+              </Text>
+              <Text>
+                Worked with JavaScript and in the React ecosystem since 2014 and have a passion for modularization and readable code.
+              </Text>
+            </ContentSectionContent>
+          </ContentSection>
+          <Divider />
+          <ContentSection>
+            <Title>Contact</Title>
+            <ContentSectionContent>
+              <Contact>
+                <ContactCards>{email}</ContactCards>
+                <ContactCards href={github}>github profile</ContactCards>
+                <ContactCards href={profileGoogleDocs}>CV</ContactCards>
+              </Contact>
+            </ContentSectionContent>  
+          </ContentSection>
+          <Divider />
+          <ContentSection>
+            <Projects />
+          </ContentSection>
+        </AppContent>
+        <Footer />
+      </AppRoot>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/Components/Themes/index.js
@@ -0,0 +1,8 @@
+export const lightTheme = {
+  background: 'rgba(245,245,245,0.7)',
+  divider: 'rgba(245,245,245,0.7)',
+}
+export const darkTheme = {
+  background: 'rgba(45,45,45,0.7)',
+  divider: 'rgba(45,45,45,0.7)',
+}
