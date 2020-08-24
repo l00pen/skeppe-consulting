e90849ab7b624955473e4998950fe2bda09f39db
l00pen <lovisa.skeppe@gmail.com>
2020-08-24 11:53:53 +0200

    Increase contrast on text

--- src/Components/Text/index.js
+++ src/Components/Text/index.js
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
 
 export const AppTitle = styled.h1`
-  color: rgba(0, 0, 0, 1);
-  color: rgba(3,2,20,0.7);
+  color: ${({ theme }) => theme.color};
   font-size: 15vw;
   font-weight: 100;
   margin: 0 auto 0.5em;
@@ -12,20 +11,20 @@ export const AppTitle = styled.h1`
 `;
 
 export const TitleRoot = styled.div`
-  color: rgba(3,2,20,0.7);
+  color: ${({ theme }) => theme.color};
   position: relative;
   width: fit-content;
   margin: 0 auto 1em;
 `;
 
 export const Title = styled.h3`
-  color: rgba(3,2,20,0.7);
+  color: ${({ theme }) => theme.color};
   font-weight: 100;
   margin-bottom: 0.2em;
 `;
 
 export const Text = styled.p`
-  color: rgba(3,2,20,0.7);
+  color: ${({ theme }) => theme.color};
   font-weight: 100;
   margin: 0.5em 0;
 `;
--- src/Components/Themes/index.js
+++ src/Components/Themes/index.js
@@ -1,8 +1,10 @@
 export const lightTheme = {
   background: 'rgba(245,245,245,0.7)',
   divider: 'rgba(245,245,245,0.7)',
+  color: 'rgba(3, 2, 20, 1)',
 }
 export const darkTheme = {
   background: 'rgba(45,45,45,0.7)',
   divider: 'rgba(45,45,45,0.7)',
+  color: 'rgba(250, 250, 250, 0.7)',
 }
